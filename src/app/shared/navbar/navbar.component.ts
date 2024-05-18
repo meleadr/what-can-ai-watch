d20355b01cd6acf74f57faf02311af95dd5fe2df
Meledina Adriano <meledina.adriano@gmail.com>
2024-05-18 15:56:46 +0200

    feat: layout navbar

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { NgClass, NgIf } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { MegaMenuItem } from 'primeng/api';
+import { RippleModule } from 'primeng/ripple';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    MegaMenuModule,
+    NgClass,
+    ButtonModule,
+    AvatarModule,
+    RippleModule,
+    NgIf,
+    InputGroupModule,
+    InputTextModule,
+  ],
+  templateUrl: './navbar.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent implements OnInit {
+  items: MegaMenuItem[] | undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Chat AI',
+        routerLink: 'chat',
+      },
+      {
+        label: 'Top films',
+      },
+      {
+        label: 'Contact',
+      },
+    ];
+  }
+}
