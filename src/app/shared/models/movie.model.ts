663103954b249f8562f04732379a21e6077dcd9e
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-05-20 15:51:41 +0200

    bugfix: show movie found

--- src/app/shared/models/movie.model.ts
+++ src/app/shared/models/movie.model.ts
@@ -3,6 +3,23 @@ import { ProductionCompany } from '@app/shared/models/production.model';
 import { Genre } from '@app/shared/models/genre.model';
 
 export class Movie {
+  constructor(movie: Movie) {
+    this.id = movie.id;
+    this.adult = movie.adult;
+    this.backdrop_path = movie.backdrop_path;
+    this.genre_ids = movie.genre_ids;
+    this.original_language = movie.original_language;
+    this.original_title = movie.original_title;
+    this.overview = movie.overview;
+    this.popularity = movie.popularity;
+    this.poster_path = movie.poster_path;
+    this.release_date = movie.release_date;
+    this.title = movie.title;
+    this.video = movie.video;
+    this.vote_average = movie.vote_average;
+    this.vote_count = movie.vote_count;
+  }
+
   id: number;
   adult: boolean;
   backdrop_path: string;
