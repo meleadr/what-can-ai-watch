d9ce5c3875831e063d85db0a57faa73babe906e7
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-05-20 13:50:16 +0200

    feat: landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [ButtonModule, RouterLink],
+  imports: [ButtonModule, RouterLink, RippleModule],
   templateUrl: './landing.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
