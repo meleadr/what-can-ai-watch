7711b65c85d729d6de2fec0f0db8846eaa1a01b9
Meledina Adriano <meledina.adriano@gmail.com>
2024-05-20 02:48:27 +0200

    feat: navbar improvement

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -3,10 +3,11 @@ import { MegaMenuModule } from 'primeng/megamenu';
 import { NgClass, NgIf } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
-import { MegaMenuItem } from 'primeng/api';
+import { MenuItem } from 'primeng/api';
 import { RippleModule } from 'primeng/ripple';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputTextModule } from 'primeng/inputtext';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-navbar',
@@ -20,12 +21,13 @@ import { InputTextModule } from 'primeng/inputtext';
     NgIf,
     InputGroupModule,
     InputTextModule,
+    MenubarModule,
   ],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent implements OnInit {
-  items: MegaMenuItem[] | undefined;
+  items: MenuItem[] | undefined;
 
   ngOnInit(): void {
     this.items = [
@@ -34,16 +36,46 @@ export class NavbarComponent implements OnInit {
         routerLink: 'chat',
       },
       {
-        label: 'Top films',
-        routerLink: 'movie/top-rated',
+        label: 'Films',
+        icon: 'pi pi-fw pi-video',
+        items: [
+          {
+            label: 'Top films',
+            icon: 'pi pi-fw pi-star',
+            routerLink: 'movie/top-rated',
+          },
+          {
+            label: 'Populaires',
+            icon: 'pi pi-fw pi-thumbs-up',
+            routerLink: 'movie/popular',
+          },
+          {
+            label: 'Prochaines sorties',
+            icon: 'pi pi-fw pi-calendar',
+            routerLink: 'movie/upcoming',
+          },
+        ],
       },
       {
-        label: 'Populaires',
-        routerLink: 'movie/popular',
-      },
-      {
-        label: 'Prochaines sorties',
-        routerLink: 'movie/upcoming',
+        label: 'Séries',
+        icon: 'pi pi-fw pi-video',
+        items: [
+          {
+            label: 'Top séries',
+            icon: 'pi pi-fw pi-star',
+            routerLink: 'movie/top-rated',
+          },
+          {
+            label: 'Populaires',
+            icon: 'pi pi-fw pi-thumbs-up',
+            routerLink: 'movie/popular',
+          },
+          {
+            label: 'Prochaines sorties',
+            icon: 'pi pi-fw pi-calendar',
+            routerLink: 'movie/upcoming',
+          },
+        ],
       },
       {
         label: 'A propos',
