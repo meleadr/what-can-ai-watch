f118db50ee50fa1bd6c123a73a9e14f866759f40
Meledina Adriano <meledina.adriano@gmail.com>
2024-05-20 03:50:18 +0200

    feat: show card on find movie

--- src/app/feature/chat/chat.component.ts
+++ src/app/feature/chat/chat.component.ts
@@ -4,6 +4,7 @@ import {
   Component,
   ElementRef,
   inject,
+  OnInit,
   QueryList,
   ViewChildren,
 } from '@angular/core';
@@ -16,6 +17,8 @@ import { InputTextModule } from 'primeng/inputtext';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from '@app/shared/pipes/filter.pipe';
+import { ImageModule } from 'primeng/image';
+import { SmallCardComponent } from '@app/feature/chat/components/small-card/small-card.component';
 
 @Component({
   selector: 'app-chat',
@@ -28,6 +31,8 @@ import { FilterPipe } from '@app/shared/pipes/filter.pipe';
     InputGroupModule,
     FilterPipe,
     FormsModule,
+    ImageModule,
+    SmallCardComponent,
   ],
   styles: [
     `
@@ -40,12 +45,33 @@ import { FilterPipe } from '@app/shared/pipes/filter.pipe';
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [ChatManager],
 })
-export class ChatComponent implements AfterViewChecked {
+export class ChatComponent implements OnInit, AfterViewChecked {
   @ViewChildren('messageDiv') private messages!: QueryList<ElementRef>;
 
   public manager = inject(ChatManager);
   public inputValue: string;
   protected readonly OpenAiRole = OpenAiRole;
+
+  ngOnInit(): void {
+    console.log('ChatComponent');
+    // this.manager.movieProposition.next({
+    //   adult: false,
+    //   backdrop_path: '/kXfqcdQKsToO0OUXHcrrNCHDBzO.jpg',
+    //   id: 278,
+    //   original_language: 'en',
+    //   genre_ids: [],
+    //   original_title: 'The Shawshank Redemption',
+    //   overview:
+    //     "En 1947, Andy Dufresne, un jeune banquier, est condamné à la prison à vie pour le meurtre de sa femme et de son amant. Ayant beau clamer son innocence, il est emprisonné à Shawshank, le pénitencier le plus sévère de l'État du Maine. Il y fait la rencontre de Red, un noir désabusé, détenu depuis vingt ans. Commence alors une grande histoire d'amitié entre les deux hommes…",
+    //   popularity: 162.204,
+    //   poster_path: '/t30GjttOdb5At1sYy8b3TOwFgWV.jpg',
+    //   release_date: '1994-09-23',
+    //   title: 'Les Évadés',
+    //   video: false,
+    //   vote_average: 8.705,
+    //   vote_count: 26178,
+    // });
+  }
 
   ngAfterViewChecked(): void {
     this.scrollToBottom();
--- src/app/feature/chat/managers/chat.manager.ts
+++ src/app/feature/chat/managers/chat.manager.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, finalize } from 'rxjs';
+import { BehaviorSubject, filter, finalize, switchMap, tap } from 'rxjs';
 import { OpenAiMessage, OpenAiRole } from '../model/chat.model';
 import { OpenAiService } from '../services/open-ai.service';
 import { MOVIE_HELPER_PROMPT } from '@app/shared/constants/prompt.constant';
+import { Movie } from '@app/shared/models/movie.model';
+import { TmdbService } from '@app/shared/services/tmdb.service';
 
 @Injectable()
 export class ChatManager {
-  public messagesOpenAi$: BehaviorSubject<OpenAiMessage[]> =
-    new BehaviorSubject<OpenAiMessage[]>([
-      {
-        role: OpenAiRole.System,
-        content: MOVIE_HELPER_PROMPT,
-      },
-    ]);
+  public messagesOpenAi: BehaviorSubject<OpenAiMessage[]> = new BehaviorSubject<
+    OpenAiMessage[]
+  >([
+    {
+      role: OpenAiRole.System,
+      content: MOVIE_HELPER_PROMPT,
+    },
+  ]);
+  public movieProposition: BehaviorSubject<Movie> = new BehaviorSubject<Movie>(
+    null
+  );
 
   private isLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
     false
   );
-
   public readonly isLoading$ = this.isLoading.asObservable();
 
-  constructor(private service: OpenAiService) {}
+  constructor(
+    private service: OpenAiService,
+    private tmdbService: TmdbService
+  ) {}
 
   public addMessage(message: OpenAiMessage): boolean {
     if (!message.content || this.isLoading.value) return false;
@@ -28,15 +36,21 @@ export class ChatManager {
     this.addOpenAiMessage(message);
 
     this.service
-      .generateResponse(this.messagesOpenAi$.value)
-      .pipe(finalize(() => this.isLoading.next(false)))
-      .subscribe(response => {
-        this.addOpenAiMessage(response);
-      });
+      .generateResponse(this.messagesOpenAi.value)
+      .pipe(
+        finalize(() => this.isLoading.next(false)),
+        tap(response => this.addOpenAiMessage(response)),
+        filter(response => response.title !== null),
+        switchMap(response =>
+          this.tmdbService.searchMovieByQuery(response.title)
+        ),
+        tap(movie => this.movieProposition.next(movie))
+      )
+      .subscribe();
     return true;
   }
 
   private addOpenAiMessage(message: OpenAiMessage): void {
-    this.messagesOpenAi$.next([...this.messagesOpenAi$.value, message]);
+    this.messagesOpenAi.next([...this.messagesOpenAi.value, message]);
   }
 }
